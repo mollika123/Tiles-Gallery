c9a97921a7041b9a54dfb6319930947d4b677e2e
mollika123 <aktermollika49@gmail.com>
2026-05-01 10:26:26 +0200

    added all products page

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -8,33 +8,37 @@ const AllTilesPage =async () => {
 console.log(tiles);
 
   return (
-    <div className="w-11/12 mx-auto space-y-5 py-8">
-      <h1 className="text-5xl text-gray-800 text-semibold mt-8">Catalog</h1>
-      <h1 className="text-gray-500">Where everyone finds inspiration: explore and let yourself be conquered!</h1>
-
-      <div className="grid md:grid-cols-2   lg:grid-cols-4 gap-6">
-        {tiles.map(tile => {
-          return (<div key={tile.id} className="shadow-xl ">
-            
-        <div className="relative  w-full h-48">    <Image src={tile.image} alt="banner"  fill
-    className="object-cover w-full"></Image></div>
-            <div className="p-5 space-y-3">
-                <h1>{tile.title}</h1>
-            <p>${tile.price}</p>
-              <p>{tile.dimensions}</p>
+   <div className="w-11/12 mx-auto space-y-5 py-8">
+          <h1 className="text-5xl text-gray-800 text-semibold mt-8">All Tiles Products</h1>
+          <h1 className="text-gray-500">Where everyone finds inspiration: explore and let yourself be conquered!</h1>
+    
+          <div className="grid md:grid-cols-2   lg:grid-cols-4 gap-6">
+            {tiles.slice(0,4).map(tile => {
+              return (<div key={tile.id} className="shadow-xl ">
+                
+            <div className="relative  w-full h-48">    <Image src={tile.image} alt="banner"  fill
+                  className="object-cover w-full"></Image>
+                  <Chip className="absolute right-2 top-2">{ tile.category}</Chip>
+                
+                </div>
+                <div className="p-5 space-y-3">
+                    <h1 className="text-xl font-bold text-gray-800">{tile.title}</h1>
+                <p>${tile.price}</p>
+                  <p>{tile.dimensions}</p>
+                  
+                  <div className="flex text-yellow-400"><FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                    <FaStar /></div>
+                  <button className="btn rounded-full w-full border bg-gray-700 py-2 font-bold text-white"><Link href="/">View Details</Link></button>
+              </div>
               
-              <div className="flex text-yellow-400"><FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar /></div>
+              </div>)
+    })}
           </div>
-          
-          </div>)
-})}
-      </div>
-
-    </div>
+    
+        </div>
   );
 };
 
