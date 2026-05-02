7d7c18c7241c3f34169104c14b98b3cc6e8134cb
mollika123 <aktermollika49@gmail.com>
2026-05-02 12:12:38 +0200

    fix some error

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -16,7 +16,7 @@ const AllTilesPage = () => {
   useEffect(() => {
     setLoading(true);
 
-    fetch("http://localhost:5000/products")
+    fetch("https://tiles-gallery-murex.vercel.app/products")
       .then((res) => res.json())
       .then((data) => {
         setTiles(data);
